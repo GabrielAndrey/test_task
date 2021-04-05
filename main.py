f547da23e7b48deed61a6370541dae085b0b85c6
GabrielAndrey <moskvitol14@yandex.ru>
2021-04-05 13:30:31 +0300

    Update main.py
    Уточнил комментарии функций, вывод для ошибок, удалил лишнее, сделал сравнение посчитанного хэша с эталонным нечувствительным к регистру.

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ def hash_check(src : str, hash_func : str, benchmark : str) -> str:
 
     src       - source file to check
     hash_func - hash algorythm
-    benchmark - correct hash"""
+    benchmark - correct hash
+
+    return    - NOT FOUND if source file doesn't exist.
+                OK if calculated hash is alike benchmark,
+                FAIL otherwise."""
     if os.path.isfile(src):
         try:
             with open(src, 'rb') as file:
@@ -18,22 +22,22 @@ def hash_check(src : str, hash_func : str, benchmark : str) -> str:
                 hash_result = hash_data_init()
                 for block in iter(lambda: file.read(4096), b""):
                     hash_result.update(block)
-                if hash_result.hexdigest() == benchmark:
+                if hash_result.hexdigest().lower() == benchmark.lower():
                     return "OK"
                 else:
                     return "FAIL"
         except FileNotFoundError:
             return "NOT FOUND"
         except PermissionError:
-            return "NOT FOUND"
+            return "Access error. Check user access rights."
 
     else:
         return "NOT FOUND"
 
 def main_processing(input_file_path : str, data_dir_path : str):
-    """Parses input and calls hash checks.
+    """Parses input, calls hash checks, print the result.
 
-    input_file_path - pat to the input file
+    input_file_path - path to the input file
     data_dir_path   - path to the directory containing the files to check"""
 
     expected_hash_functions = ('md5', 'sha1', 'sha256')
@@ -47,8 +51,11 @@ def main_processing(input_file_path : str, data_dir_path : str):
             with open(input_file_path, 'r') as input_file:
                 for line in input_file:
                     input_arguments = list(filter(None, line.strip().split()))
-                    if len(input_arguments) != 3:
-                        print("Bad parameters.")
+                    if len(input_arguments) < 3:
+                        print("Not enough parameters. Should be filename, hash algorithm and calculated hash.")
+                        continue
+                    elif len(input_arguments) > 3:
+                        print(f"{input_arguments[0]} Too many parameters. Should be filename, hash algorithm and calculated hash.")
                         continue
                     if not input_arguments[1] in expected_hash_functions:
                         print(f"{input_arguments[0]} Bad parameters, hash function is not supported.")
@@ -72,6 +79,4 @@ if __name__ == "__main__":
     if (sys.argv[0] == '') or (len(sys.argv) < 3):
         print("Bad parameters passed to the program.\n")
     else:
-        input_file = sys.argv[1]
-        input_dir  = sys.argv[2]
-        main_processing(input_file, input_dir)
+        main_processing(sys.argv[1], sys.argv[2])
